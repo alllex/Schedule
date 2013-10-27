ba6a859df38de28c6070a18d6398c9925c9e8e88
Alex Semin <Alllex.Semin@gmail.com>
2013-10-27 12:13:22 +0300

    Initial state

---
+++ SEE/ExcelIntegrationTests/Program.fs
@@ -0,0 +1,14 @@
+﻿open ExcelDna.Integration
+
+[<ExcelFunction(Description="Increment")>]
+let dnaInc x = x + 1
+
+[<ExcelFunction(Description="Change string in starnge way")>]
+let dnaChangeStr s = "[prefix<" + s + ">postfix]"
+
+[<ExcelFunction(Description="Multiply numbers")>]
+let dnaMult x y = x * y
+
+// take an array as argument
+[<ExcelFunction(Description="Sum array elements")>]
+let dnaSumArr (xs : double[]) = Array.fold (+) 0.0 xs   
