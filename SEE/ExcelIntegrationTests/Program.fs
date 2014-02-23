713f3af59cd4012f1643631004c3b810be061f15
Alex Semin <Alllex.Semin@gmail.com>
2014-02-24 00:07:30 +0400

    Minor changes

--- SEE/ExcelIntegrationTests/Program.fs
+++ SEE/ExcelIntegrationTests/Program.fs
@@ -14,7 +14,13 @@ let dnaMult x y = x * y
 let dnaSumArr (xs : double[]) = Array.fold (+) 0.0 xs
 
 // take an array2D as argument
-[<ExcelFunction(Description="Sum array elements")>]
+[<ExcelFunction(Description="Pretty description")>]
 let dnaSumArr2d (xs : double[,]) = 
     xs |> Seq.cast<double>
        |> Seq.fold (+) 0.0
+
+[<ExcelFunction(Description="Test")>]
+let dnaTest () = 
+  try
+    XlCall.Excel(XlCall.xlSet, 0, 2, ExcelReference(0,0), 7).ToString()
+  with | _ -> "fail..."
