354fa2e9935e493039e9ddf3b98f7bf2353e955d
Alex Semin <Alllex.Semin@gmail.com>
2014-03-18 17:30:03 +0400

    Rewrite tested import and export

--- SEE/ExcelIntegration/Program.fs
+++ SEE/ExcelIntegration/Program.fs
@@ -1,54 +1,45 @@
 ﻿
+namespace SEE
 
 open Microsoft.Office.Interop.Excel
 open System.IO
 
-// Run Excel as a visible application
-let app = new ApplicationClass(Visible = false) 
+type TransferData = (string [,]) list
 
-// Create new file and get the first worksheet
-let workbook = app.Workbooks.Open(@"M:\projects\Experiments\FSharp\Schedule\SEE\ExcelIntegration\bin\Debug\b.xlsx",
-                                   Editable = true)
-// Note that worksheets are indexed from one instead of zero
-let worksheet = (workbook.Worksheets.[1] :?> Worksheet)
+type Importer = 
+    class
+        static member Import(path : string) : TransferData = 
+            let getWorksheet (worksheet : Worksheet) =
+                let usedRange = worksheet.UsedRange
+                let rows = usedRange.Rows.Count
+                let cols = usedRange.Columns.Count
+                printfn "WS: %Ax%A" rows cols
+                Array2D.init rows cols
+                             (fun i j -> string <| (worksheet.Cells.[i + 1, j + 1] :?> Range).Value2)
+            let app = new ApplicationClass(Visible = false)
+            let workbook = app.Workbooks.Open(path, Editable = true)
+            let worksheetsCount = workbook.Worksheets.Count
+            printfn "worksheets count = %A" worksheetsCount
+            let worksheets = [ for x in workbook.Worksheets -> 
+                                    let ws = x :?> Worksheet
+                                    getWorksheet ws
+                             ]
+            app.Quit()
+            worksheets
+    end
 
-let xlRange = worksheet.UsedRange
-let rowCount = xlRange.Rows.Count
-let colCount = xlRange.Columns.Count
-
-printfn "rowCount %A" rowCount
-printfn "colCount %A" colCount
-
-let data = Array2D.create rowCount colCount "*"
-
-for i in 1..rowCount do
-    for j in 1..colCount do
-        data.[i - 1, j - 1] <- string <| (worksheet.Cells.[i, j] :?> Range).Value2
-
-let fileName = "output.txt"
-
-let out = new StreamWriter(fileName)
-
-for i in 0..rowCount - 1 do
-    for j in 0..colCount - 1 do
-        out.WriteLine(sprintf "[%A, %A] == %A" i j data.[i, j])
-
-out.Close()
-
-let ws2 = (workbook.Worksheets.[2] :?> Worksheet)
-
-open System
-
-// Store data in arrays of strings or floats
-let rnd = new Random()
-let titles = [| "No"; "Maybe"; "Yes" |]
-let names = Array2D.init 10 1 (fun i _ -> string('A' + char(i)))
-let data' = Array2D.init 10 3 (fun _ _ -> rnd.NextDouble())
-
-// Populate data into Excel worksheet
-ws2.Range("C2", "E2").Value2  <- titles
-ws2.Range("B3", "B12").Value2 <- names
-ws2.Range("C3", "E12").Value2 <- data'
-
-workbook.Save()
-app.Quit()
+type Exporter =
+    class
+        static member Export(path : string, data : TransferData) =
+            let app = new ApplicationClass(Visible = false)
+            let workbook = app.Workbooks.Add(XlWBATemplate.xlWBATWorksheet)
+            List.iter
+                (fun table ->
+                    let ws = workbook.Worksheets.Add() :?> Worksheet
+                    for i in 1..Array2D.length1 table do
+                        for j in 1..Array2D.length2 table do
+                            (ws.Cells.[i, j] :?> Range).Value2 <- table.[i - 1, j - 1]
+                ) data
+            workbook.SaveAs(Filename = path)
+            app.Quit()
+    end
--- SEE/ExcelIntegration/Tests.fsx
+++ SEE/ExcelIntegration/Tests.fsx
@@ -1,37 +1,9 @@
 ﻿
 #r "Microsoft.Office.Interop.Excel.dll"
+#load "Program.fs"
 open Microsoft.Office.Interop.Excel
-
-// Run Excel as a visible application
-let app = new ApplicationClass(Visible = false) 
-
-// Create new file and get the first worksheet
-let workbook = app.Workbooks.Open(@"C:\a.xlsx")
-// Note that worksheets are indexed from one instead of zero
-let worksheet = (workbook.Worksheets.[1] :?> Worksheet)
-
-let xlRange = worksheet.UsedRange
-let rowCount = xlRange.Rows.Count
-let colCount = xlRange.Columns.Count
-
-printfn "rowCount %A" rowCount
-printfn "colCount %A" colCount
-
-let data = Array2D.create rowCount colCount "*"
-
-for i in 1..rowCount do
-    for j in 1..colCount do
-        data.[i - 1, j - 1] <- string <| (worksheet.Cells.[i, j] :?> Range).Value2
-
-let fileName = "output.txt"
-
+open SEE
 open System.IO
 
-let out = new StreamWriter(fileName)
-
-for i in 0..rowCount - 1 do
-    for j in 0..colCount - 1 do
-        out.Write(data.[i, j])
-    out.WriteLine()
-
-out.Close()
+let data = Importer.Import(@"M:\projects\Experiments\FSharp\Schedule\SEE\SheduleSample.xlsx")
+Exporter.Export(@"M:\projects\Experiments\FSharp\Schedule\SEE\SheduleSampleExported.xlsx", data)
