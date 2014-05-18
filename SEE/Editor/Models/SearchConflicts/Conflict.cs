73ebd878fda810c3487964600b3ede76791a8f9e
Лозов Пётр <not_kill@mail.ru>
2014-05-18 14:22:35 +0400

    Add first conflict

---
+++ SEE/Editor/Models/SearchConflicts/Conflict.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Editor.Models.SearchConflicts
+{
+    public enum ConflictType { Warning, Conflict }
+
+    public struct Conflict
+    {
+        public String Message;
+        public ConflictType ConflictType;
+        public IEnumerable<Class> ConflictingClasses;
+
+        public Conflict(String message, ConflictType conflictType, IEnumerable<Class> conflictingClasses)
+        {
+            Message = message;
+            ConflictType = conflictType;
+            ConflictingClasses = conflictingClasses;
+        }
+    }
+}
---
+++ SEE/Editor/Models/SearchConflicts/SearchConflicts.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Editor.Models.SearchConflicts
+{
+    using Conflicts = List<Conflict>;
+
+    class SearchConflicts
+    {
+        public static Conflicts ConflictsGroups(ClassesSchedule schedule)
+        {
+            var conflicts = new Conflicts();
+            conflicts.AddRange(GreatestFourClassesPerDay(schedule));
+            return conflicts;
+        }
+
+        public static Conflicts GreatestFourClassesPerDay(ClassesSchedule schedule)
+        {
+            var conflicts = new Conflicts();
+
+            var groupClassesAll = from c in schedule.Classes
+                                  group c by new Tuple<Group, Weekdays>(c.Group, c.ClassTime.Day);
+
+            var groupClassesOdd = from g in groupClassesAll
+                                  select g.Where(c => c.ClassTime.Week == WeekType.Odd ||
+                                                      c.ClassTime.Week == WeekType.Both);
+            
+            var groupClassesEven = from g in groupClassesAll
+                                   select g.Where(c => c.ClassTime.Week == WeekType.Even ||
+                                                       c.ClassTime.Week == WeekType.Both);
+
+            foreach (IEnumerable<Class> c in groupClassesOdd)
+            {
+                if (c.Count() >= 5) conflicts.Add(
+                    new Conflict("Больше 4х занятий в день", ConflictType.Warning, c));
+            }
+
+            foreach (IEnumerable<Class> c in groupClassesEven)
+            {
+                if (c.Count() >= 5) conflicts.Add(
+                    new Conflict("Больше 4х занятий в день", ConflictType.Warning, c));
+            }
+
+            return conflicts;
+        }
+
+        public static Conflicts GroupsInDifferentClassrooms(ClassesSchedule schedule)
+        {
+            return null; // Заглушка
+        }
+    }
+}
