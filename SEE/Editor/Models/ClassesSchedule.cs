3de76fb8e4cb83c79a7bb334c4f8451ec4ffcd37
Alex Semin <Alllex.Semin@gmail.com>
2014-05-19 22:04:15 +0700

    Complete merging

--- SEE/Editor/Models/ClassesSchedule.cs
+++ SEE/Editor/Models/ClassesSchedule.cs
@@ -1,6 +1,8 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Collections.Specialized;
+using System.Linq;
 using Editor.Helpers;
 using Editor.Models.SerializableModels;
 
