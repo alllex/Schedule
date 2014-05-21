9046872c161dd4859071bff1c7e02f9119f64fe5
Alex Semin <Alllex.Semin@gmail.com>
2014-05-21 13:50:35 +0400

    Unchanged group order in tables

--- SEE/Editor/ViewModels/Helpers/TitlesMarkup.cs
+++ SEE/Editor/ViewModels/Helpers/TitlesMarkup.cs
@@ -13,10 +13,7 @@ namespace Editor.ViewModels.Helpers
         {
             var headers =
                 from t in groups
-                orderby t.Name
-                group t by t.Specialization
-                into h
-                select h;
+                group t by t.Specialization;
 
             int currectCol = 0;
             foreach (var h in headers)
