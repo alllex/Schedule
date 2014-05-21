acf1839dd407696930b862ff46be22dc8b2b875c
Лозов Пётр <not_kill@mail.ru>
2014-05-21 18:43:00 +0400

    Added import and export methods.

---
+++ SEE/Editor/Models/ImportAndExport.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SEE;
+
+namespace Editor.Models
+{
+    class ImportAndExport
+    {
+        public static void Export(ClassesSchedule schedule, string path)
+        {
+            Exporter.Export(path, sClassesSchedule.Create(schedule));
+        }
+
+        public static ClassesSchedule Import(string path)
+        {
+            return null; // Заглушка
+        }
+    }
+}
