6362acc728bf98f0d21d820fef4e9249a2b048fb
Alex Semin <Alllex.Semin@gmail.com>
2014-05-27 02:06:36 +0800

    Add ImportExport file

---
+++ SEE/Editor/Models/ImportExport.cs
@@ -0,0 +1,32 @@
+﻿using ScheduleData;
+using SEE;
+
+namespace Editor.Models
+{
+    class ScheduleExporter
+    {
+        public static void ExcelExport(Schedule schedule, string path)
+        {
+            ExcelExporter.Export(path, schedule);
+        }
+
+        public static void DatabaseExport(Schedule schedule, string path)
+        {
+            SQLiteDatabaseIO.Save(schedule, path);
+        }
+
+    }
+
+    class ScheduleImporter
+    {
+        public static Schedule ExcelImport(string path)
+        {
+            return ExcelImporter.Import(path);
+        }
+
+        public static Schedule DatabaseImport(string fileName)
+        {
+            return SQLiteDatabaseIO.Load(fileName);
+        }
+    }
+}
